ac42557eb129bf7132c202ed786033f0e053647c
Guilherme Mulato <guilherme_mulato@hotmail.com>
2021-02-09 23:33:11 -0300

    Implementacao de envio de email pela AWS SES

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+    driver: 'ethereal' | 'ses';
+
+    defaults: {
+        from: {
+            email: string;
+            name: string;
+        };
+    };
+}
+
+export default {
+    driver: process.env.MAIL_DRIVER || 'ethereal',
+
+    defaults: {
+        from: {
+            email: 'guilherme@mulato.com',
+            name: 'Guilherme Mulato',
+        },
+    },
+} as IMailConfig;
